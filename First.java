17376afdb34452f344e5e901f770331e8290db5c
Jaithrabose <jaithra.bose@gmail.com>
2014-10-08 15:28:18 +0530

    updation

--- First.java
+++
@@ -1,9 +0,0 @@
-import java.net.*;
-import java.rmi.*;
-public interface First extends Remote
-{
-	public void cmpr(String s1,String s2)throws RemoteException;
-	public void rvs(String s1)throws RemoteException;
-	public void ct(String s1,String s2)throws RemoteException;
-	public void cs(String s1,String s2)throws RemoteException;
-}
